3ce6b0208e9818b9e8c64ca60c6e1a301ecfd57a
JordanKornel <78873469+JordanKornel@users.noreply.github.com>
2022-11-30 12:53:41 +0100

    Add files via upload

---
+++ DWWUAP_1130/ObjectDWWUAP/src/ObjectDWWUAP/JSObject.java
@@ -0,0 +1,11 @@
+package ObjectDWWUAP;
+
+public class JSObject {
+
+    public void put(String string, String string2) {
+    }
+
+    public void put(String string, int i) {
+    }
+
+}
---
+++ DWWUAP_1130/ObjectDWWUAP/src/ObjectDWWUAP/ObjectDWWUAP.java
@@ -0,0 +1,14 @@
+package ObjectDWWUAP;
+
+public class ObjectDWWUAP {
+    public static void main(String[] args) {
+        JSObject obj = new JSObject();
+
+        obj.put("nev", "Jordan Kornel");
+        obj.put("neptun", "DWWUAP");
+        obj.put("szak", "PTI");
+
+        System.out.print(obj);
+
+    }
+}
